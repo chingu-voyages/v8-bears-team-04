8a3686b766cd53e5f0c98bdcbc2301bfc236e1e3
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-28 14:56:09 +0100

    feat(server): Add navigation for user authentication

--- server/app.js
+++ server/app.js
@@ -2,12 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const { commonalities } = require('./components/common/commonMiddlewares');
-const commonRoutes = require('./components/common/commonRoutes');
+const { commonalities } = require('./middleware/commonMiddlewares');
+const commonRoutes = require('./routes/commonRoutes');
+const UserAuth = require('./routes/UserAuth');
 
 app.use(commonalities);
 // custom backend routes
 app.use(commonRoutes);
+
+//user authentication
+app.use('/auth', UserAuth)
 
 // for each unknown path it will send back the index of 
 // react app
---
+++ server/controllers/UserCtrl.js
@@ -0,0 +1,14 @@
+
+class UserCtrl {
+	static Login(req,res) {
+		res.send('login')
+	}
+
+	static Register(req,res) {
+		res.send('register')
+	}
+
+}
+
+
+module.exports = UserCtrl
