a22986ba8a3e887ca2b3c0a8ff3a9c992ca0bab0
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-03-28 15:47:23 +0100

    feat(server): Add database for user authentication

--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -1,11 +1,63 @@
+const User = require('../models/UserAuth')
 
 class UserCtrl {
+
 	static Login(req,res) {
-		res.send('login')
+		User.findOne(
+			{email: req.body.email},(err,user)=>{
+				if(err){
+					throw err
+				}
+				if(!user) {
+					return res.json({
+						message: 'Invalid username or password'
+					})
+				}
+				if(user) {
+					return res.json({
+						message: 'Login Success'
+					})
+				}
+		})
+	
 	}
 
+
 	static Register(req,res) {
-		res.send('register')
+
+		User.findOne(
+			{email: req.body.email}, (err,user)=>{
+				if(err){
+					throw err
+				}
+				if(user) {
+					return res.json({
+						message: 'User already exists'
+					})
+				}else {
+					const user = new User({
+						name: req.body.name,
+						email: req.body.email, 
+						password: req.body.password
+					})
+
+					//save user
+					user.save((err,user)=>{
+						if(err){
+							return res.json({
+								error: 'Unable to register user at this time'
+							})
+						
+						}else{
+							return res.json({
+								message: 'User Registered'
+							})
+						}
+
+					})
+				}
+
+		})
 	}
 
 }
