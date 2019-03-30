ad8ed9b4472b84b7ee7999c40bc41b3209a88956
R-Layer <layer@europe.com>
2019-03-30 16:40:54 +0100

    fixed doubled file

--- server/middleware/commonmiddlewares.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const express = require('express');
-
-exports.commonalities = [
-    express.static(path.join(__dirname, 'client/build')),
-    express.json(),
-    express.urlencoded({extended: false}),
-];
