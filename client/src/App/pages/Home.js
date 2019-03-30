7acdfa03bcb714483d33bebd1499c330a7781249
R-Layer <layer@europe.com>
2019-03-30 16:40:16 +0100

    fixed Map implementation

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Map from '../components/interactive-map/InteractiveMap';
 
 function Home (props) {
     const [message, setMessage] = useState('No response');
