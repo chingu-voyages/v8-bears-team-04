4a8bdaa6b534dd7cd45918f50acd33fa8e285e6a
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-04-17 21:27:54 +0100

    ft-passport user authentication

--- server/models/UserAuth.js
+++ server/models/UserAuth.js
@@ -1,10 +1,16 @@
+
 const { Schema, model } = require('mongoose')
+const passportLocalMongoose = require('passport-local-mongoose')
+
 
 const UserSchema = new Schema({
-	name: String,
+	username: String,
 	email: String,
 	password: String
+}, {
+	timestamps: true
 })
 
+UserSchema.plugin(passportLocalMongoose)
 
 module.exports = model('User', UserSchema)
--- server/routes/UserAuth.js
+++ server/routes/UserAuth.js
@@ -5,12 +5,10 @@ const router = express.Router()
 const UserCtrl = require('../controllers/UserCtrl')
 
 router.route('/login')
-.get(UserCtrl.Login)
 .post(UserCtrl.Login)
 
 
 router.route('/register')
-.get(UserCtrl.Register)
 .post(UserCtrl.Register)
 
 
