59aec67d85d54c7137d857d78a3705fca09804d8
R-Layer <layer@europe.com>
2019-04-27 21:05:01 +0200

    add validation on front - login form

--- client/src/App/components/forms/SigninForm.js
+++ client/src/App/components/forms/SigninForm.js
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
+import {emailValidation, passwordValidation} from './validationRules';
 
 import './forms.scss';
 
 function SigninForm({submitData}) {
 
     const [email, setEmail] = useState("");
+    const [emailError, setEmailError] = useState(null);
     const [password, setPassword] = useState("");
+    const [passwordError, setPasswordError] = useState(null);
 
     function submitForm(e) {
         e.preventDefault();
 
         submitData({email, password});
         setEmail("");
-        setPassword("");        
+        setPassword("");
+    }
+
+    function validateEmail(email) {
+        let validationError = emailValidation(email);
+        if(validationError) {
+            setEmailError(validationError);
+        }
+    };
+
+    function validatePassword(password) {
+        let validationError = passwordValidation(password);
+        if(validationError) {
+            setPasswordError(validationError);
+        }
     }
 
     return ( 
@@ -25,7 +42,10 @@ function SigninForm({submitData}) {
                     size="1"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
+                    onBlur={e => validateEmail(e.target.value)}
+                    onFocus={() => setEmailError(null)}
                 />
+                <small>{emailError}</small>
             </div>
             <div className="form-field" >
                 <label htmlFor="password"> Password: </label>
@@ -35,7 +55,10 @@ function SigninForm({submitData}) {
                     size="1"
                     value={password}
                     onChange={e => setPassword(e.target.value)}
+                    onBlur={e => validatePassword(e.target.value)}
+                    onFocus={() => setPasswordError(null)}
                 />
+                <small>{passwordError}</small>
             </div>
             <div className="form-field" >
                 <input 
