fdb6b8f3fe55a8c048f9d2460c82f91c952c46bc
R-Layer <layer@europe.com>
2019-04-27 21:03:15 +0200

    remove map from home

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Map from '../components/interactive-map/InteractiveMap';
 import Notification from '../components/notification/Notification';
 
 function Home (props) {
@@ -10,14 +9,13 @@ function Home (props) {
         
     return (
         <div className="App">
-            <Notification data={{type, message:msg}} />}
+            <Notification data={{type, message:msg}} />
             <div className="hero">
                 <div className="hero-text" >
                     <h1 className="hero-title" >LOCATION FINDER</h1>
                     <h4 className="hero-subtitle">REPLACE WITH AN INSPIRING TEXT THAT DESCRIBE OUR APP</h4>
                 </div>
             </div>
-           {/*  <Map /> */}
         </div>
     );
 };
