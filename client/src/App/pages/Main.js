cebf060ccf2351257c7a00ea240c974a629faae0
R-Layer <layer@europe.com>
2019-04-29 16:39:30 +0200

    extract modal from main page

--- client/src/App/pages/Main.js
+++ client/src/App/pages/Main.js
@@ -1,18 +1,13 @@
 import React from 'react';
 
 import Tabs from '../components/tabs/Tabs';
-import Modal from '../components/modal/Modal';
-import useModal from '../components/modal/useModal';
 
 function Main() {
 
-    const {isOpen, toggle} = useModal();
-
     return (
-        <div>
+        <div className="map-container">
             <div className="placeholder"></div>
-            <Tabs toggleModal={toggle} />
-            <Modal close={toggle} isOpen={isOpen} />
+            <Tabs />
         </div>
     );
 };
