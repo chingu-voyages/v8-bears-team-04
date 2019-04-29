9a6f9641e2305fdff56c585b5f8f70b4474c8095
R-Layer <layer@europe.com>
2019-04-29 16:44:43 +0200

    add modal to tabs ( each tab )

--- client/src/App/components/tabs/MarkTab.js
+++ client/src/App/components/tabs/MarkTab.js
@@ -1,21 +1,43 @@
 import React from 'react';
 
-function MarkTab({toggleModal}) {
+import Modal from '../modal/Modal';
+import useModal from '../modal/useModal';
+
+import ModalFormCoordinates from '../forms/ModalFormCoordinates';
+
+function MarkTab() {
+
+    const {isOpen, toggle} = useModal();
+
+    function submitData(lat, lng) {
+        console.log('lat: ', lat);
+        console.log('lng: ', lng);
+    }
+
     return(
         <>
             <div className="row">
-                <button className="add-button">
+                <button 
+                    className="add-button row-button"
+                >
                     Add mark at your location
                 </button>
             </div>
             <div className="row">
                 <button 
-                    className="add-button"
-                    onClick={toggleModal}
+                    className="add-button row-button"
+                    onClick={toggle}
                 >
                     Add mark at specific coordinates
                 </button>
             </div>
+            <Modal 
+                close={toggle} 
+                isOpen={isOpen} 
+                title={"Mark coordinates"}
+            >
+               <ModalFormCoordinates submitData={submitData}/>
+            </Modal>
         </>
     );
 };
--- client/src/App/components/tabs/SearchTab.js
+++ client/src/App/components/tabs/SearchTab.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Modal from '../modal/Modal';
+import useModal from '../modal/useModal';
+
+import ModalFormCoordinates from '../forms/ModalFormCoordinates';
+import ModalFormZone from '../forms/ModalFormZone';
 
 function SearchTab() {
+
+    const {isOpen, toggle} = useModal();
+    const [currentModal, setCurrentModal] = useState("");
+
+    function setModal(modalForm) {
+        setCurrentModal(modalForm);
+        toggle();
+    };
+
+    function submitDataLL(lat, lng) {
+        console.log('lat: ', lat);
+        console.log('lng: ', lng);
+    };
+
+    function submitDataZone(zone) {
+        console.log('zone: ', zone);
+    };
+
     return(
         <>
             <div className="row">
-                <button className="add-button">
-                    Search for a business
+                <button
+                    className="theme-secondary-button row-button"
+                    onClick={() => setModal("name")}
+                >
+                    Search places in a specific zone
                 </button>
             </div>
             <div className="row">
-                <button className="add-button">
+                <button
+                    className="theme-secondary-button row-button"
+                    onClick={() => setModal("LL")}
+                >
                     Go to coordinates
                 </button>
             </div>
+            <Modal 
+                close={toggle} 
+                isOpen={isOpen} 
+                title={"Search Places"}
+            >
+              {currentModal === "LL" 
+              ? <ModalFormCoordinates submitData={submitDataLL}/>
+              : <ModalFormZone submitData={submitDataZone} />}
+            </Modal>
         </>
     );
 };
